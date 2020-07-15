c13307a9a0ab5778a1cbe6b98bb3904816400739
DanielCortild <daniel.cortild@gmail.com>
2020-07-15 09:44:05 +0200

    Added External Server

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import Axios from 'axios';
-const backServer = 'http://localhost:3000'
-//const backServer = 'https://edifly-server.herokuapp.com/';
+//const backServer = 'http://localhost:3000'
+const backServer = 'https://edifly-server.herokuapp.com/';
 
 export const fetchClients = async () => {
   let {data} = await Axios.get(`${backServer}/clients.json`);
