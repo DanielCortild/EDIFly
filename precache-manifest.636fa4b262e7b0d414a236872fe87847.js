0774c479af87081c8aa074974756063d0ce27899
DanielCortild <daniel.cortild@gmail.com>
2020-07-18 02:08:39 +0200

    Updates

---
+++ precache-manifest.636fa4b262e7b0d414a236872fe87847.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d79e217f2e3e672d0fa23080b4cb8509",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c687c059304db692ebfd",
+    "url": "/static/css/2.70db59fb.chunk.css"
+  },
+  {
+    "revision": "fac4dc8c9f0373769888",
+    "url": "/static/css/main.260569a0.chunk.css"
+  },
+  {
+    "revision": "c687c059304db692ebfd",
+    "url": "/static/js/2.faf5c9d6.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.faf5c9d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fac4dc8c9f0373769888",
+    "url": "/static/js/main.84fe9249.chunk.js"
+  },
+  {
+    "revision": "034ace13aa46ba6d2e5e",
+    "url": "/static/js/runtime-main.764ac3b2.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  }
+]);
