2dcf4fb37f6899f91b4aa8d54ac0db57fb705bf8
DanielCortild <daniel.cortild@gmail.com>
2020-07-30 11:48:24 +0200

    Updates

---
+++ precache-manifest.11d4e5b63b6907fb3f1dbe3146d758a7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb609cf9f410633995e706b92766e78f",
+    "url": "/EDIfly/index.html"
+  },
+  {
+    "revision": "78dd4c3ef8399049ff82",
+    "url": "/EDIfly/static/css/2.eb1425ec.chunk.css"
+  },
+  {
+    "revision": "68eaa31f00c823137b1a",
+    "url": "/EDIfly/static/css/main.a920b1a3.chunk.css"
+  },
+  {
+    "revision": "78dd4c3ef8399049ff82",
+    "url": "/EDIfly/static/js/2.112bbafc.chunk.js"
+  },
+  {
+    "revision": "b6ecc2ae71044227721a6052aea6820a",
+    "url": "/EDIfly/static/js/2.112bbafc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68eaa31f00c823137b1a",
+    "url": "/EDIfly/static/js/main.d33f84ce.chunk.js"
+  },
+  {
+    "revision": "88954c6828a5aaf41fb4",
+    "url": "/EDIfly/static/js/runtime-main.af777bc0.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/EDIfly/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/EDIfly/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/EDIfly/static/media/revicons.2feb69cc.eot"
+  }
+]);
