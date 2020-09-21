2192bf218b060c70fd480099ddee02e4db7fc50a
DanielCortild <daniel.cortild@gmail.com>
2020-09-21 19:30:07 +0200

    Cannot just download

--- src/components/S3_Support.js
+++ src/components/S3_Support.js
@@ -3,16 +3,19 @@ import {Container, Row, Col, Button, Modal} from 'reactstrap';
 import {AvForm, AvField} from 'availity-reactstrap-validation';
 import {downloadFiles, postDownloadedFile} from '../api';
 
-const DownloadModal = ({file, value, close}) => {
+const DownloadModal = ({file, value, close, downloaded, setDownloaded}) => {
   let {title, filename, file_url} = file || {};
-  const handleValidSubmit = async (event, values) => {
+
+  const handleValidSubmit = async (_, values) => {
     postDownloadedFile({
       name: values.name,
       company: values.company,
       email: values.email,
-      file: filename
+      file: filename,
+      file_url: file_url
     });
-    downloadFiles(filename, file_url);
+    //downloadFiles(filename, file_url);
+    setDownloaded(true);
   }
 
   return (
@@ -27,6 +30,7 @@ const DownloadModal = ({file, value, close}) => {
         </div>
 
         <div className="modal-body mb-1 p-0">
+        {!downloaded ? 
           <AvForm onValidSubmit={handleValidSubmit}>
             <div className="px-5 py-3">
               <Row>
@@ -44,6 +48,7 @@ const DownloadModal = ({file, value, close}) => {
               <Button color="secondary" onClick={close}>Cancel</Button>
             </div>
           </AvForm>
+        : <h4 className="p-3 pt-0">Thank you for downloading this file. A link has been sent to your email address.</h4>}
         </div>
       </Modal>
     </div>
@@ -53,17 +58,19 @@ const DownloadModal = ({file, value, close}) => {
 export default ({data: {downloads}}) => {
   const [modal, setModal] = useState();
   const [modalOpen, setModalOpen] = useState();
+  const [downloaded, setDownloaded] = useState(false);
 
   const openModal = (whitepaper) => {
-    setModal(whitepaper)
-    setModalOpen(true)
+    setModal(whitepaper);
+    setModalOpen(true);
+    setDownloaded(false);
   }
   const closeModal = () => setModalOpen(false);
 
   return (
     <section className="section px-3 px-md-0" id="support" style={{background: '#7cb3d8', color: 'white'}}>
 
-      <DownloadModal value={modalOpen} close={closeModal} file={modal} />
+      <DownloadModal value={modalOpen} close={closeModal} file={modal} downloaded={downloaded} setDownloaded={setDownloaded} />
 
       <Container className="text-center">
         <h2 className="title mb-5" style={{color: 'white'}}>EDIfly Solution Support</h2>
